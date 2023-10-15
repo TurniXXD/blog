a0f66498235971405fb01f26a8dcafbdcb403c90
TurniXXD <legokubik@seznam.cz>
2023-10-16 01:45:04 +0200

    fix: build

--- remix.config.js
+++ remix.config.js
@@ -5,6 +5,7 @@ module.exports = {
   serverBuildTarget: "vercel",
   server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: [".*"],
+  browserNodeBuiltinsPolyfill: { modules: { fs: true, path: true } },
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
